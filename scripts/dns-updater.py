7e94c47c2a0ce18d7cf45d7b0ccb174cf4940be8
Russell Heilling <russell@linx.net>
2014-08-11 15:25:05 +0100

    Reorganise module.  get rid of rgh prefix

--- scripts/dns-updater.py
+++ scripts/dns-updater.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 'Detect when local IP has changed and update DNS via web API'
-from rgh.dnsupdater import main
+from dnsupdater import main
 
 if __name__ == '__main__':
   main.run()
